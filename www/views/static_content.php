5a71194489dd94e115129f1d763948ea3e59bf12
Fisher Evans <contact@fisherevans.com>
2015-06-08 16:01:02 -0400

    escaping rel links in static contnet

--- www/views/static_content.php
+++ www/views/static_content.php
@@ -4,6 +4,6 @@ $parseDown = new ParsedownExtra();
 <div class="section" itemscope itemtype="http://schema.org/<?=$staticContent['itemtype']?>">
   <h1 itemprop="name"><?=$staticContent['name']?></h1>
   <div itemprop="text">
-    <?=$parseDown->text($staticContent['content'])?>
+    <?=fixRelativeLinks($parseDown->text($staticContent['content']))?>
   </div>
 </div>
