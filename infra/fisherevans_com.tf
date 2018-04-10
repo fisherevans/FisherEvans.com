4b4a67615cc130d357e0b8827c2bfd48d6bf7340
Fisher Evans <fisher.evans@dealertrack.com>
2018-04-09 21:40:04 -0400

    add dns and distrinution infra

--- infra/fisherevans_com.tf
+++ infra/fisherevans_com.tf
@@ -1,9 +1,14 @@
 
-variable "region" { default = "us-east-1" }
+variable "region"     { default = "us-east-1" }
 variable "rootDomain" { default = "fisherevans.com" }
+variable "sslCertArn" { default = "arn:aws:acm:us-east-1:658675223305:certificate/93b48056-2f0e-43ce-a490-d700cbbea7a6" }
 
 provider "aws" {
   region = "${var.region}"
+}
+
+resource "aws_route53_zone" "fisherevansHostedZone" {
+   name = "${var.rootDomain}."
 }
 
 resource "aws_s3_bucket" "fisherevansBucket" {
@@ -31,3 +36,16 @@ resource "aws_s3_bucket_policy" "publicReadPolicy" {
   bucket = "${aws_s3_bucket.fisherevansBucket.id}"
   policy = "${data.aws_iam_policy_document.hostedContentReadPolicy.json}"
 }
+
+module "hosted-www" {
+  source = "components/hosted_subdomain"
+  
+  hostedZone = "${aws_route53_zone.fisherevansHostedZone.zone_id}"  
+  domain = "${var.rootDomain}"
+  subdomain = "www"
+  sslCertArn = "${var.sslCertArn}"
+  
+  bucket = "${aws_s3_bucket.fisherevansBucket.id}"
+  keyPrefix = "hosted-content/www"
+}
+
