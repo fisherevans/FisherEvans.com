6e675d33ccb18c368c3ef0efa9e836c75fbdd085
Fisher Evans <contact@fisherevans.com>
2018-04-15 11:42:27 -0400

    tf: update cf distro to redirect HTTP to HTTPS

--- infra/terraform/components/hosted_subdomain/main.tf
+++ infra/terraform/components/hosted_subdomain/main.tf
@@ -26,7 +26,7 @@ resource "aws_cloudfront_distribution" "distribution" {
       query_string = false
       cookies { forward = "none" }
     }
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl = 0
     default_ttl = 30
     max_ttl = 86400
